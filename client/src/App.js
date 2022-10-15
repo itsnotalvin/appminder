7a02cadd9bce35c2d8caee8aef8a6fda0c60a453
Alvin Yip <alvin.y97@gmail.com>
2022-10-15 14:09:23 +1100

    testing heroku

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This project will be a portfolio
         </a>
       </header>
     </div>
