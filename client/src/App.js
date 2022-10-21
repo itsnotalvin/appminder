d0c53de2e4ea9794369f40149efbbf4fbf191f02
Alvin Yip <alvin.y97@gmail.com>
2022-10-21 22:38:11 +1100

    added work on login signup pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Login }from './components/Login.js';
+import { Signup } from './components/Signup.js';
 
 function App() {
   return (
     <div className="App">
       <h1>This is a portfolio website</h1>
       <h2>Body</h2>
+      <Login />
+      <Signup />
     </div>
   );
 }
