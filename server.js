bc73de68825aa80f7e801b11b5616ee6b00b4589
Alvin Yip <alvin.y97@gmail.com>
2022-10-22 16:26:15 +1100

    added Login module, server dependencies

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const db = require('./database/db.js')
 const port = process.env.PORT || 3001;
 
 const pg = require('pg');
@@ -9,6 +9,19 @@ const usersController = require('./controller/users.js')
 
 const jobsController = require('./controller/Jobs.js');
 
+const { expressSession, pgSession } = require('./session');
+
+app.use(
+    expressSession({
+        store: new pgSession({
+            pool: db,
+            createTableIfMissing: true,
+        }),
+        secret: process.env.EXPRESS_SESSION_SECRET_KEY,
+        resave: false,
+        saveUninitialized: false
+    })
+);
 
 
 
@@ -17,6 +30,8 @@ app.use('/jobs', jobsController);
 app.use(express.json());
 app.use(express.static("./client/build"));
 
-app.use(`/users`, usersController)
+
+
+app.use('/users', usersController);
 
 app.listen(port, () => console.log(`Listening at localhost:${port}`));
