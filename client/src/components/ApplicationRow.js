caf010e304ded35c511ee65b56202969b9d975e9
Siddharth Singh <siddharth.s1@outlook.com>
2022-10-22 19:49:48 +1100

    Changed some css to make the job rows stand out; made the job-drop-down functionality work - need to work on next stage of it

--- client/src/components/ApplicationRow.js
+++ client/src/components/ApplicationRow.js
@@ -1,13 +1,47 @@
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import { useState, useEffect } from 'react';
+
 export const ApplicationRow = ({ jobInfo }) => {
+    const expandedStyle = {
+        display: 'flex'
+    };
+
+    const collapsedStyle = {
+        display: 'none'
+    };
+    const [hideJobDetails, setHideJobDetails] = useState(false);
     const { id, job_title, company_name, app_stage, key_date, archived, completed, deleted, last_updated, notes, set_reminder } = jobInfo;
+
+    const expandJobDetails = (id) => {
+        console.log(id);
+        setHideJobDetails(hideJobDetails ? false : true);
+    };
+
     return (
         <div className='job-row'>
-            <div>{company_name}</div>
-            <div>{job_title}</div>
-            <div>{key_date}</div>
-            <div>{last_updated}</div>
-            <div>{'<>'}</div>
-            <div>Archive</div>
+            <div className='core-job-details'>
+                <div>{company_name}</div>
+                <div>{job_title}</div>
+                <div>{key_date}</div>
+                <div>{last_updated}</div>
+                <div>
+                    {
+                        !hideJobDetails ? <ArrowDropDownIcon className='icon-mod' onClick={() => expandJobDetails(id)} /> : <ArrowDropUpIcon className='icon-mod' onClick={() => expandJobDetails(id)} />
+                    }
+                </div>
+                <div><ArchiveIcon className='icon-mod' /></div>
+            </div>
+            <div style={{ display: !hideJobDetails ? 'none' : 'flex' }} className='expanded-job-content' id={id}>
+                <div>
+                    <strong style={{ margin: 0, padding: 0 }}>Notes</strong>
+                </div>
+                <div className='notes-and-appUpdateBtn'>
+                    <span>{notes}</span>
+                    <span className='application-btn'>Update Application</span>
+                </div>
+            </div>
         </div>
     )
 };
