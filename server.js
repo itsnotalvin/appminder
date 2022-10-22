60e1dda237633ae30689fbfd196f1b6d1d9efd79
Alvin Yip <alvin.y97@gmail.com>
2022-10-22 11:54:35 +1100

    Added controller/users, sessions

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const port = process.env.PORT || 3001;
 
 const pg = require('pg');
 const bcrypt = require('bcrypt');
+const usersController = require('./controller/users.js')
 
 
 app.use(express.json());
 app.use(express.static("./client/build"));
 
-app.get("/api/test", (req, res) => res.json({result: "ok"}));
+app.use(`/users`, usersController)
+
+// app.get("/api/test", (req, res) => res.json({result: "ok"}));
 
 app.listen(port, () => console.log(`Listening at localhost:${port}`));
