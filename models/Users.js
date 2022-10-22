92c9179b8818661a45768a8ecf473d0eea56699a
Siddharth Singh <siddharth.s1@outlook.com>
2022-10-22 11:39:06 +1100

    Added more functions to Jobs controller

--- models/Users.js
+++ models/Users.js
@@ -5,9 +5,9 @@ const User = {
         const sql = 'SELECT * FROM users WHERE email = $1';
         return db.query(sql, [email])
     },
-    signUp: (first_name, last_name, email, hashed_pw, sec_qns, sec_ans, reminder_days) => {
-        const sql = 'INSERT INTO users(first_name,last_name,email,hashed_pw,sec_qns,sec_ans,reminder_days) VALUES($1,$2,$3,$4,$5,$6,$7)';
-        return db.query(sql, [first_name, last_name, email, hashed_pw, sec_qns, sec_ans, reminder_days])
+    signUp: (first_name, last_name, email, hashed_pw, sec_qns, sec_ans) => {
+        const sql = 'INSERT INTO users(first_name,last_name,email,hashed_pw,sec_qns,sec_ans,reminder_days) VALUES($1,$2,$3,$4,$5,$6,3)';
+        return db.query(sql, [first_name, last_name, email, hashed_pw, sec_qns, sec_ans])
     },
     checkPassword: (email) => {
         const sql = 'SELECT hashed_pw FROM users WHERE email = $1';
