57857272057badf77829853cbf9f70bc4370e3d5
Siddharth Singh <siddharth.s1@outlook.com>
2022-10-23 19:47:11 +1100

    Made the date and app stage format and prefill as well; made some css changes so notes can overflow properly

--- client/src/components/ApplicationsView.js
+++ client/src/components/ApplicationsView.js
@@ -3,13 +3,14 @@ import { useState, useEffect } from "react";
 import '../Dashboard.css';
 import '../ApplicationModals.css';
 import axios from 'axios';
+import { timestampCleanup } from "./TimestampCleanup";
 
 export const ApplicationsView = () => {
     const [jobInfo, setJobInfo] = useState([]);
     const [selectedTab, setSelectedTab] = useState('Draft');
     const [infoChange, setInfoChange] = useState(0);
-    const [viewUpdateAppModal, setViewUpdateAppModal] = useState(false);
     const [modalClass, setModalClass] = useState('modal');
+    const [appToUpdateInfo, setAppToUpdateInfo] = useState({});
     useEffect(() => {
         axios.get('/jobs/allUserJobs')
             .then(res => {
@@ -19,9 +20,16 @@ export const ApplicationsView = () => {
     const changedJobInfo = () => {
         setInfoChange(infoChange === 0 ? 1 : 0);
     };
-    const updateAppModal = (newStatus) => {
-        setViewUpdateAppModal(newStatus);
+    const updateAppModal = ({ key_date, notes, app_stage }) => {
+        setAppToUpdateInfo({
+            key_date: timestampCleanup(key_date, 'update'),
+            notes: notes,
+            app_stage: app_stage
+        })
         setModalClass('modal view-modal');
+    };
+    const updateAppAction = () => {
+        setModalClass('modal');
     };
     return (
         <>
@@ -56,17 +64,26 @@ export const ApplicationsView = () => {
                 <div className='modal-box'>
                     <div className='modal-content'>
                         <h3>Update Application</h3>
-                        <select name="app-stage" id="app-stage-dropdown">
-                            <option value='Draft'>Draft</option>
-                            <option value='Applied'>Applied</option>
-                            <option value='Interviewing'>Interviewing</option>
-                            <option value='Awaiting'>Awaiting</option>
-                        </select>
-                        <input placeholder='Key Date' />
-                        <input placeholder='Edit Notes' />
+                        <div className='update-app-field'>
+                            <label htmlFor="app_stage_dropdown">Application Stage</label>
+                            <select name="app-stage" id="app_stage_dropdown" value={appToUpdateInfo.app_stage}>
+                                <option value='Draft'>Draft</option>
+                                <option value='Applied'>Applied</option>
+                                <option value='Interviewing'>Interviewing</option>
+                                <option value='Awaiting'>Awaiting</option>
+                            </select>
+                        </div>
+                        <div className='update-app-field'>
+                            <label htmlFor="key_date">Key Date</label>
+                            <input value={appToUpdateInfo.key_date} placeholder='Key Date' id='key_date' type='date' />
+                        </div>
+                        <div className='update-app-field'>
+                            <label htmlFor="notes">Notes</label>
+                            <textarea value={appToUpdateInfo.notes} placeholder='Edit Notes' id='notes' />
+                        </div>
                         <div className='update-app-btn-group'>
-                            <span>Update Application</span>
-                            <span>Discard Update</span>
+                            <span onClick={updateAppAction}>Update Application</span>
+                            <span onClick={() => setModalClass('modal')}>Discard Update</span>
                         </div>
                     </div>
                 </div>
--- client/src/components/TimestampCleanup.js
+++ client/src/components/TimestampCleanup.js
@@ -1,11 +1,31 @@
-export const timestampCleanup = (timestamp) => {
+export const timestampCleanup = (timestamp, key) => {
     const formatted = String(new Date(Date.parse(timestamp)));
     const formattedArr = formatted.split(' ');
     const time = formattedArr[4].split(':');
-    if (parseInt(time[0]) > 12) {
-        return `${formattedArr[1]} ${formattedArr[2]} ${formattedArr[3]} ${parseInt(time[0]) - 12}:${time[1]} PM`
+    const monthNumMap = {
+        'Jan': 1,
+        'Feb': 2,
+        'Mar': 3,
+        'Apr': 4,
+        'May': 5,
+        'Jun': 6,
+        'Jul': 7,
+        'Aug': 8,
+        'Sep': 9,
+        'Oct': 10,
+        'Nov': 11,
+        'Dec': 12
+    };
+
+    if (key === 'update') {
+        return `${formattedArr[3]}-${monthNumMap[formattedArr[1]]}-${formattedArr[2]}`
     }
     else {
-        return `${formattedArr[1]} ${formattedArr[2]} ${formattedArr[3]} ${parseInt(time[0])}:${time[1]} AM`
+        if (parseInt(time[0]) > 12) {
+            return `${formattedArr[1]} ${formattedArr[2]} ${formattedArr[3]} ${parseInt(time[0]) - 12}:${time[1]} PM`
+        }
+        else {
+            return `${formattedArr[1]} ${formattedArr[2]} ${formattedArr[3]} ${parseInt(time[0])}:${time[1]} AM`
+        }
     }
 };
