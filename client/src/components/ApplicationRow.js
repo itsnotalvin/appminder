3f9919f15f4db3cc2511322956ef53ecb311b9e7
Siddharth Singh <siddharth.s1@outlook.com>
2022-10-23 12:02:10 +1100

    Added a reminder icon to application row component - added state so it toggles with every click - linking DB call next

--- client/src/components/ApplicationRow.js
+++ client/src/components/ApplicationRow.js
@@ -3,13 +3,22 @@ import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import ArchiveIcon from '@mui/icons-material/Archive';
 import { useState, useEffect } from 'react';
 import { timestampCleanup } from './TimestampCleanup';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 
 export const ApplicationRow = ({ jobInfo }) => {
     const [hideJobDetails, setHideJobDetails] = useState(false);
+    const [emptyBell, setEmptyBell] = useState('icon-mod hidden');
+    const [bell, setBell] = useState('icon-mod');
     const { id, job_title, company_name, app_stage, key_date, archived, completed, deleted, last_updated, notes, set_reminder } = jobInfo;
 
     const expandJobDetails = (id) => {
         setHideJobDetails(hideJobDetails ? false : true);
+    };
+
+    const updateBell = () => {
+        setEmptyBell(emptyBell === 'icon-mod' ? 'icon-mod hidden' : 'icon-mod');
+        setBell(bell === 'icon-mod hidden' ? 'icon-mod' : 'icon-mod hidden');
     };
 
     const lastUpdatedDateTime = timestampCleanup(last_updated);
@@ -21,6 +30,11 @@ export const ApplicationRow = ({ jobInfo }) => {
                 <div>{job_title}</div>
                 <div>{key_date.split('T')[0]}</div>
                 <div>{lastUpdatedDateTime}</div>
+                <div>
+                    {
+                        set_reminder ? (<div onClick={updateBell}><NotificationsActiveIcon className={bell} /><NotificationsNoneIcon className={emptyBell} /></div>) : (<div onClick={updateBell}><NotificationsNoneIcon className={emptyBell} /><NotificationsActiveIcon className={bell} /></div>)
+                    }
+                </div>
                 <div>
                     {
                         !hideJobDetails ? <ArrowDropDownIcon className='icon-mod' onClick={() => expandJobDetails(id)} /> : <ArrowDropUpIcon className='icon-mod' onClick={() => expandJobDetails(id)} />
