b5a261655ad53c946acabd9d7b6ae5dfa3fd2dd2
Siddharth Singh <siddharth.s1@outlook.com>
2022-10-26 19:51:23 +1100

    Fixed DND bug - dnd works perfectly now

--- client/src/components/JobDetails.js
+++ client/src/components/JobDetails.js
@@ -28,7 +28,7 @@ export const JobDetails = ({ jobs, selected, changedJobInfo, updateAppModal }) =
     return (
         <div id="stage-type-applications">
             {
-                displayJobs.length ? displayJobs.map((job, index) => <ApplicationRow key={index} jobInfo={job} changeReminderStatus={changeReminderStatus} changeArchiveStatus={changeArchiveStatus} updateAppModal={() => updateAppModal(job)} />) : <div>Nothing to show!</div>
+                displayJobs.length ? displayJobs.map((job, index) => <ApplicationRow key={job.id} jobInfo={job} changeReminderStatus={changeReminderStatus} changeArchiveStatus={changeArchiveStatus} updateAppModal={() => updateAppModal(job)} />) : <div>Nothing to show!</div>
             }
         </div>
     )
