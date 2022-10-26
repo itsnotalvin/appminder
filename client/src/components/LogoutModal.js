4e40c1e15525c9c8645159ed2cd50d9b6d4b4a58
Alvin Yip <alvin.y97@gmail.com>
2022-10-26 22:09:43 +1100

    mini changes to logout

--- client/src/components/LogoutModal.js
+++ client/src/components/LogoutModal.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
 import axios from 'axios'
-import { Link } from 'react-router-dom'
+import { redirect } from 'react-router-dom'
 
 const LOGOUT_URL = '/sessions'
 
 
 export const LogoutModal = () => {
+    
+    const [errMsg, setErrMsg] = useState('')
+    const [loggedout, setLoggedout] = useState(false)
+
+
     const logout = (e) => {
         e.preventDefault();
         console.log('you are trying to log out')
 
-        axios.delete('/users/session')
-        
-        // try {
-        //     const response = await axios.delete(LOGOUT_URL);
-        // } catch (err) {
-        //     setErrMsg('Unable to log out')
-        // }
+        // axios.delete('/users/session')
+           
+        try {
+            console.log('you have logged out')
+            axios.delete('/users/session')
+            .then(res => {
+                redirect('/')
+            })
+        } catch (err) {
+            setErrMsg('Unable to log out')
+        }
     }
     return (
         <div>
@@ -32,6 +41,9 @@ export const LogoutModal = () => {
             <div>
                 
                 <button onClick={logout}>Yes</button>
+                
+                
+                
                 
                 <button>No</button>
             </div>
