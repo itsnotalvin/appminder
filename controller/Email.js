00e0f68dff7a2165add7e03483f1006845424ef0
Siddharth Singh <siddharth.s1@outlook.com>
2022-10-28 13:34:54 +1100

    Made the email functionality work - when a user signs up, a welcome email gets sent out to them

--- controller/Email.js
+++ controller/Email.js
@@ -2,16 +2,24 @@ const emailTransporter = require('../utility/Email.js');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (request, response) => {
+router.post('/send', (request, response) => {
+    const { email } = request.body;
     let mailOptions = {
-        from: 'test@gmail.com',
-        to: 'ss007jb@gmail.com',
-        subject: 'Nodemailer API Test',
-        text: 'Hi from your nodemailer API'
+        from: process.env.EMAIL,
+        to: email,
+        subject: 'Welcome to App-Minder!',
+        text: 'Hello from your App-Minder team! Glad to have you with us, and Good Luck with your job applications! Always remember - Fortune favours the bold!'
     };
 
     emailTransporter.sendMail(mailOptions, (err, data) => {
-        err ? console.log("Error => ", err) : console.log('Email sent successfully!')
+        if (err) {
+            console.log('Error => ', err);
+            response.status(500).json({ message: 'Due to an internal server error, we could not send your email!' });
+        }
+        else {
+            console.log('Email sent successfully!');
+            response.json({ message: 'Email sent successfully!' })
+        }
     });
 });
 
